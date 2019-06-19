77b801bf2479cf44636837f4115812ac6eff9b87
mengxinglai <1254133613@qq.com>
2019-06-19 21:30:59 +0800

    修改generatorConfig.xml文件

--- src/main/java/com/shineyue/dao/StudentMapper.java
+++ src/main/java/com/shineyue/dao/StudentMapper.java
@@ -2,43 +2,40 @@ package com.shineyue.dao;
 
 import com.shineyue.model.Student;
 
-import java.util.List;
-
 public interface StudentMapper {
     /**
      *
-     * @mbg.generated 2019-06-19 20:08:00
+     * @mbg.generated 2019-06-19 21:29:55
      */
     int deleteByPrimaryKey(String t_id);
 
     /**
      *
-     * @mbg.generated 2019-06-19 20:08:00
+     * @mbg.generated 2019-06-19 21:29:55
      */
     int insert(Student record);
 
     /**
      *
-     * @mbg.generated 2019-06-19 20:08:00
+     * @mbg.generated 2019-06-19 21:29:55
      */
     int insertSelective(Student record);
 
     /**
      *
-     * @mbg.generated 2019-06-19 20:08:00
+     * @mbg.generated 2019-06-19 21:29:55
      */
     Student selectByPrimaryKey(String t_id);
 
     /**
      *
-     * @mbg.generated 2019-06-19 20:08:00
+     * @mbg.generated 2019-06-19 21:29:55
      */
     int updateByPrimaryKeySelective(Student record);
 
     /**
      *
-     * @mbg.generated 2019-06-19 20:08:00
+     * @mbg.generated 2019-06-19 21:29:55
      */
     int updateByPrimaryKey(Student record);
-    List<Student> selectStudent();
 }
